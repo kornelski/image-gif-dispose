3484c874d65569bc7c287c155ea2bd16359e7537
Kornel <kornel@geekhood.net>
2020-11-03 19:00:51 +0000

    Rustier enum

--- src/disposal.rs
+++ src/disposal.rs
@@ -1,10 +1,15 @@
-use gif::DisposalMethod::*;
+use gif::DisposalMethod;
 use imgref::*;
 use std::default::Default;
 
+enum SavedState<Pixel> {
+    Previous(Vec<Pixel>),
+    Background,
+    Keep,
+}
+
 pub struct Disposal<Pixel> {
-    method: gif::DisposalMethod,
-    previous_pixels: Option<Vec<Pixel>>,
+    saved: SavedState<Pixel>,
     left: u16, top: u16,
     width: u16, height: u16,
 }
@@ -12,39 +17,39 @@ pub struct Disposal<Pixel> {
 impl<Pixel: Copy + Default> Default for Disposal<Pixel> {
     fn default() -> Self {
         Disposal {
-           method: gif::DisposalMethod::Keep,
-           previous_pixels: None,
+           saved: SavedState::Keep,
            top: 0, left: 0, width: 0, height: 0,
        }
    }
 }
 
 impl<Pixel: Copy + Default> Disposal<Pixel> {
-    pub fn dispose(&mut self, mut pixels: ImgRefMut<'_, Pixel>) {
+    pub fn dispose(&self, mut pixels: ImgRefMut<'_, Pixel>) {
         if self.width == 0 || self.height == 0 {
             return;
         }
 
         let mut dest = pixels.sub_image_mut(self.left.into(), self.top.into(), self.width.into(), self.height.into());
-        match self.method {
-            Background => {
+        match &self.saved {
+            SavedState::Background => {
                 let bg = Pixel::default();
                 for px in dest.pixels_mut() { *px = bg; }
             },
-            Previous => if let Some(saved) = self.previous_pixels.take() {
+            SavedState::Previous(saved) => {
                 for (px, &src) in dest.pixels_mut().zip(saved.iter()) { *px = src; }
             },
-            Keep | Any => {},
+            SavedState::Keep => {},
         }
     }
 
     pub fn new(method: gif::DisposalMethod, left: u16, top: u16, width: u16, height: u16, pixels: ImgRef<'_, Pixel>) -> Self {
         Disposal {
-            previous_pixels: match method {
-                Previous => Some(pixels.sub_image(left.into(), top.into(), width.into(), height.into()).pixels().collect()),
-                _ => None,
+            saved: match method {
+                DisposalMethod::Previous => SavedState::Previous(pixels.sub_image(left.into(), top.into(), width.into(), height.into()).pixels().collect()),
+                DisposalMethod::Background => SavedState::Background,
+                _ => SavedState::Keep,
             },
-            method, left, top, width, height,
+            left, top, width, height,
         }
     }
 }
